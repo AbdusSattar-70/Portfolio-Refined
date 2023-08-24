963e5e321716a5dc079f85f21386f70ef7f6ffaf
Abdus Sattar <sasorganicagro@gmail.com>
2023-08-24 20:22:53 +0600

    Modify app.js according to the review

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
-import RecentWork from './components/pages/RecentWork';
 import Contact from './components/pages/Contact';
 import About from './components/pages/About';
 import NavBar from './components/NavBar';
 
 const App = () => (
-  <main className="mainContainer">
-    <section className="subcontainer ">
+  <main>
+    <section>
       <Router>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/recent_work" element={<RecentWork />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
         </Routes>
